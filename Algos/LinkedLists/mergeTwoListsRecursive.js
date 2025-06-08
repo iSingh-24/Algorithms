aebfcfde7c2372198eaf4cf6f171061e74747990
Inderprit Singh <singhinderprit1992@gmail.com>
2025-06-07 23:17:42 -0400

    initial setup with merge two linked Lists recursively

--- Algos/LinkedLists/mergeTwoListsRecursive.js
+++ Algos/LinkedLists/mergeTwoListsRecursive.js
@@ -2,4 +2,18 @@
  * Here will be the function that recursively sorts and merges two linked lists
  */
 
-const mergeTwoListsRecursive = (list1, list2) => {};
+const mergeTwoListsRecursive = (list1, list2) => {
+  //we will return whichever one is the remaining
+
+  if (!list1 || !list2) {
+    return list1 || list2;
+  }
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoListsRecursive(list1.next, list2);
+  } else {
+    list1.next = mergeTwoListsRecursive(list1, list2.next);
+  }
+
+  return list1;
+};
