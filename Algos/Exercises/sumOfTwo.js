21edf1c55630b7f2e8ae171cbc26b63c53838117
Inderprit Singh <singhinderprit1992@gmail.com>
2025-06-18 18:22:31 -0400

    added a way to find sum with two pointers

--- Algos/Exercises/sumOfTwo.js
+++ Algos/Exercises/sumOfTwo.js
@@ -9,18 +9,45 @@ let arr = [1, 2, 3, 4, 5];
 
 //Brute Force Method, quadratic time complexity n²
 
-const sumOfTwo = (arr, target) => {
-  for (let i = 0; i < arr.length; i++) {
-    let firstNum = arr[i];
-    for (let j = i + 1; j < arr.length; j++) {
-      let secondNum = arr[j];
+// const sumOfTwo = (arr, target) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     let firstNum = arr[i];
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let secondNum = arr[j];
 
-      if (firstNum + secondNum === target) {
-        console.log(
-          `The first number is ${firstNum} and the second number is ${secondNum}`
-        );
-        return true;
-      }
+//       if (firstNum + secondNum === target) {
+//         console.log(
+//           `The first number is ${firstNum} and the second number is ${secondNum}`
+//         );
+//         return true;
+//       }
+//     }
+//   }
+
+//   return false;
+// };
+
+//Two pointers
+
+const sumOfTwo = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    let sum = arr[start] + arr[end];
+
+    if (sum === target) {
+      console.log(
+        `There is a pair, the values are ${arr[start]} and ${arr[end]}`
+      );
+
+      return true;
+    }
+
+    if (sum < target) {
+      start++;
+    } else {
+      end--;
     }
   }
 
