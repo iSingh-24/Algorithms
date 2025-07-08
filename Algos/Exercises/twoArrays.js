98009a1ad0cd4c298c51da61a08d465bb0a03e1e
Inderprit Singh <singhinderprit1992@gmail.com>
2025-07-08 14:04:47 -0400

    brute force way set with quadratic runtime

--- Algos/Exercises/twoArrays.js
+++ Algos/Exercises/twoArrays.js
@@ -2,6 +2,23 @@
  * Prompt: Given two arrays, check to see if either of the arrays have any common elements
  */
 
-const twoArrays = () => {
+//first try brute force
+
+const twoArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    let firstNum = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      let secondNum = arr2[j];
+      if (firstNum === secondNum) return true;
+    }
+  }
+
   return false;
 };
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+const arr2 = [8, 9, 10, 11];
+const arr3 = [5, 8];
+
+console.log(twoArrays(arr1, arr2));
+console.log(twoArrays(arr1, arr3));
