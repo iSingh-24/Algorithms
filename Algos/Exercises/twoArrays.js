a3dec185a6dc78f23bac42c00242bd119dab1da0
Inderprit Singh <singhinderprit1992@gmail.com>
2025-07-08 14:35:11 -0400

    used a map to check for overlapping values within the two arrays

--- Algos/Exercises/twoArrays.js
+++ Algos/Exercises/twoArrays.js
@@ -4,13 +4,45 @@
 
 //first try brute force
 
+// const twoArrays = (arr1, arr2) => {
+//   for (let i = 0; i < arr1.length; i++) {
+//     let firstNum = arr1[i];
+//     for (let j = 0; j < arr2.length; j++) {
+//       let secondNum = arr2[j];
+//       if (firstNum === secondNum) return true;
+//     }
+//   }
+
+//   return false;
+// };
+
+//Second way
+
+// const twoArrays = (arr1, arr2) => {
+//   let set1 = new Set(arr1);
+
+//   for (let i = 0; i < arr2.length; i++) {
+//     let currNum = arr2[i];
+
+//     if (set1.has(currNum)) return true;
+//   }
+
+//   return false;
+// };
+
+//third way
+
 const twoArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    let firstNum = arr1[i];
-    for (let j = 0; j < arr2.length; j++) {
-      let secondNum = arr2[j];
-      if (firstNum === secondNum) return true;
-    }
+  const arr1Map = new Map();
+
+  for (let [index, num] of Object.entries(arr1)) {
+    arr1Map.set(num, index);
+  }
+
+  //map.get will return undefined if value isn't present, while map.has will return a boolean value
+
+  for (let num of arr2) {
+    if (arr1Map.has(num)) return true;
   }
 
   return false;
